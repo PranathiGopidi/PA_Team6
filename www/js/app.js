9bf185f8cd3958fb7ddb9078fac89f336fb22fc9
Pranathi Gopidi <pranathireddy.1704@gmail.com>
2017-03-15 23:09:17 -0500

    commit

---
+++ www/js/app.js
@@ -0,0 +1,51 @@
+// Ionic Starter App
+
+// angular.module is a global place for creating, registering and retrieving Angular modules
+// 'starter' is the name of this angular module example (also set in a <body> attribute in index.html)
+// the 2nd parameter is an array of 'requires'
+var example = angular.module('starter', ['ionic','ngCordova'])
+
+.run(function($ionicPlatform) {
+  $ionicPlatform.ready(function() {
+    // Hide the accessory bar by default (remove this to show the accessory bar above the keyboard
+    // for form inputs)
+    if(window.cordova && window.cordova.plugins.Keyboard) {
+      cordova.plugins.Keyboard.hideKeyboardAccessoryBar(true);
+    }
+    if(window.StatusBar) {
+      StatusBar.styleDefault();
+    }
+  });
+});
+example.controller("ExampleController", function($scope, $cordovaContacts) {
+ 
+    $scope.sendEmail = function() {
+        // 1
+        var bodyText = "<h2>Look at this images!</h2>";
+        if (null != $scope.images) {
+            var images = [];
+            var savedImages = $scope.images;
+            for (var i = 0; i < savedImages.length; i++) {
+                // 2
+                images.push("" + $scope.urlForImage(savedImages[i]));
+                // 3
+                images[i] = images[i].replace('file://', '');
+            }
+             
+            // 4
+            window.plugin.email.open({
+                to:          ["saimon@devdactic.com"], // email addresses for TO field
+                cc:          Array, // email addresses for CC field
+                bcc:         Array, // email addresses for BCC field
+                attachments: images, // file paths or base64 data streams
+                subject:    "Just some images", // subject of the email
+                body:       bodyText, // email body (for HTML, set isHtml to true)
+                isHtml:    true, // indicats if the body is HTML or plain text
+            }, function () {
+                console.log('email view dismissed');
+            },
+            this);    
+        }
+}
+ 
+});
